2853966ca2a4460e19ef5199101bd35aeea84ecb
yazzumi <horiuchiryuta@gmail.com>
2025-10-31 19:30:08 +0800

    p

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
